e67fac919295c9ac852d024c46dbfb94236dbd42
guice_lin <guice_lin@126.com>
2016-11-11 18:17:48 +0800

    add portal

---
+++ src/main/java/com/dazi/spa/portal/CheckResultController.java
@@ -0,0 +1,87 @@
+package com.dazi.spa.portal;
+
+import com.dazi.spa.common.datatable.Order;
+import com.dazi.spa.common.utils.IntegerUtil;
+import com.dazi.spa.modules.checkItem.model.CheckItem;
+import com.dazi.spa.modules.checkItem.service.CheckItemService;
+import com.dazi.spa.modules.client.model.CheckRecord;
+import com.dazi.spa.modules.client.model.CheckResult;
+import com.dazi.spa.modules.client.service.CheckRecordService;
+import com.dazi.spa.modules.client.service.CheckResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @desc:
+ * @author:linguozhi@52tt.com
+ * @date: 2016/11/11
+ */
+@Controller
+@RequestMapping("portal")
+public class CheckResultController {
+
+    @Autowired
+    private CheckResultService checkResultService;
+
+    @Autowired
+    private CheckRecordService checkRecordService;
+
+    @Autowired
+    private CheckItemService checkItemService;
+
+    @RequestMapping("/checkResult")
+    @ResponseBody
+    public String checkResult(CheckResult checkResult, int start, int length, Model model) {
+        Assert.isTrue(IntegerUtil.gtZero(checkResult.getClientId()), "请选择查看用户");
+
+        // 获取用户最近的检测记录
+        CheckRecord latestCheckRecord = null;
+        if(IntegerUtil.gtZero(checkResult.getRecordId())){
+            latestCheckRecord = checkRecordService.selectByPrimaryKey(checkResult.getRecordId());
+        } else {
+            latestCheckRecord = checkRecordService.getLatest(checkResult.getClientId());
+        }
+
+        Assert.notNull(latestCheckRecord, "找不到该客户的检测记录");
+
+        // 查看父品项
+        CheckItem topCheckItem = null;
+        if(IntegerUtil.gtZero(checkResult.getItemId())) {
+            topCheckItem = checkItemService.selectByPrimaryKey(checkResult.getItemId());
+        } else {
+            CheckItem record = new CheckItem();
+            record.setParentId(0);
+            List<CheckItem> topCheckItemList = checkItemService.selectList(record, Order.build("order_no", "asc"), start, length);
+            topCheckItem = CollectionUtils.isEmpty(topCheckItemList) ? null : topCheckItemList.get(0);
+        }
+
+        Assert.notNull(topCheckItem, "找不到检测项");
+
+        // 查找父品项检测成绩
+        CheckResult checkResultRecord = new CheckResult();
+        checkResultRecord.setRecordId(latestCheckRecord.getId());
+        checkResultRecord.setItemId(topCheckItem.getId());
+        List<CheckResult> topCheckResultList = checkResultService.selectList(checkResultRecord, null, 0, 1);
+
+        CheckResult topCheckResult = CollectionUtils.isEmpty(topCheckResultList) ? null : topCheckResultList.get(0);
+
+        // 查找子品项检测成绩
+        checkResultRecord = new CheckResult();
+        checkResultRecord.setRecordId(latestCheckRecord.getId());
+        checkResultRecord.setParentId(topCheckItem.getId());
+
+        List<CheckResult> childCheckResultList = checkResultService.selectList(checkResultRecord, null, 0, -1);
+
+        model.addAttribute("topCheckResult", topCheckResult);
+        model.addAttribute("childCheckResultList", childCheckResultList);
+        return null;
+    }
+}
