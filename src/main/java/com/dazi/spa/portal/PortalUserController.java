9497ff93abcc8c99cf8a48eaf1b726a6f9401816
lingz <lingz@ucweb.com>
2017-03-06 21:52:15 +0800

    md

---
+++ src/main/java/com/dazi/spa/portal/PortalUserController.java
@@ -0,0 +1,37 @@
+package com.dazi.spa.portal;
+
+import com.dazi.spa.common.protocol.ResponseHelper;
+import com.dazi.spa.common.shiro.token.manager.TokenManager;
+import com.dazi.spa.console.BaseController;
+import com.dazi.spa.modules.user.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * Created by lingz on 17/2/24.
+ */
+@Controller
+@RequestMapping("portal/user")
+public class PortalUserController extends BaseController {
+    private static final Logger logger = LoggerFactory.getLogger(PortalUserController.class);
+    /**
+     * 登录
+     */
+    @RequestMapping("/submitLogin")
+    @ResponseBody
+    public Map submitLogin(User user, HttpServletRequest request) {
+        try {
+            TokenManager.login(user, true);
+            return ResponseHelper.buildSuccessResult("登录成功");
+        } catch (Exception ex) {
+            logger.error("登录错误", ex);
+            return ResponseHelper.buildErrorResult("登录失败");
+        }
+    }
+}
